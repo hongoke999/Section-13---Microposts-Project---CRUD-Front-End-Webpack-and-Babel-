78c31ec713d282e6f752937a1a1cc9fe9980a9b4
Chaka Hongoke <hongoke999@yahoo.com>
2020-06-19 17:29:57 +0300

    Add Posts & Show Alert

--- src/http.js
+++ src/http.js
@@ -1,15 +1,15 @@
 /**
- *  EasyHTTP Library
- *  Library for making HTTP requests
+ * EasyHTTP Library
+ * Library for making HTTP requests
  *
- *  @version 3.0.0
- *  @author Chaka Hongoke
- *  @license MIT
+ * @version 3.0.0
+ * @author  Brad Traversy
+ * @license MIT
  *
- */
+ **/
 
 class EasyHTTP {
-  //  Make an HTTP GET Request
+  // Make an HTTP GET Request 
   async get(url) {
     const response = await fetch(url);
     const resData = await response.json();
@@ -21,23 +21,26 @@ class EasyHTTP {
     const response = await fetch(url, {
       method: 'POST',
       headers: {
-        'Content-type': 'application/json',
+        'Content-type': 'application/json'
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(data)
     });
+
     const resData = await response.json();
     return resData;
+   
   }
 
-  // Make an HTTP PUT Request
-  async put(url, data) {
+   // Make an HTTP PUT Request
+   async put(url, data) {
     const response = await fetch(url, {
       method: 'PUT',
       headers: {
-        'Content-type': 'application/json',
+        'Content-type': 'application/json'
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(data)
     });
+    
     const resData = await response.json();
     return resData;
   }
@@ -47,12 +50,16 @@ class EasyHTTP {
     const response = await fetch(url, {
       method: 'DELETE',
       headers: {
-        'Content-type': 'application/json',
-      },
+        'Content-type': 'application/json'
+      }
     });
+
     const resData = await 'Resource Deleted...';
     return resData;
   }
-}
 
-export const http = new EasyHTTP();
+ }
+
+ export const http = new EasyHTTP();
+
+ 
