f98709fcaa6cb48ed04b6bbd1e3e311af3d1d31f
Chaka Hongoke <hongoke999@yahoo.com>
2020-06-20 10:32:09 +0300

    Post Edit State & Update[2]

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ document.querySelector('#posts').addEventListener('click', deletePost);
 //  Listen for edit state
 document.querySelector('#posts').addEventListener('click', enableEdit);
 
+//  Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 // Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -24,20 +27,43 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
   const data = {
-    title,    //  ES6 - Same as title: title,
-    body      //  ES6 - Same as body: body 
-  }
+    title, //  ES6 - Same as title: title,
+    body, //  ES6 - Same as body: body
+  };
 
-  // Create Post
-  http.post('http://localhost:3000/posts', data)
-    .then(data => {
-      ui.showAlert('Post added', 'alert alert-success');
-      ui.clearFields();
-      getPosts();
-    })
-    .catch(err => console.log(err));
+  //  Validate Inputs
+  if(title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger');
+  } else {
+    //  Check for ID
+    if(id === '') {
+
+    //  Create Post
+    http.post('http://localhost:3000/posts', data)
+      .then((data) => {
+        ui.showAlert('Post added', 'alert alert-success');
+        ui.clearFields();
+        getPosts();
+      })
+      .catch((err) => console.log(err));
+    } else {
+      //  Update Post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+      .then((data) => {
+        ui.showAlert('Post added', 'alert alert-success');
+        ui.changeFormState('add');
+        getPosts();
+      })
+      .catch((err) => console.log(err));
+    }
+    
+
+    // Create Post
+    
+  }  
 }
 
 //  Delete Post
@@ -71,6 +97,14 @@ function enableEdit(e) {
 
     //  Fill form with current post
     ui.fillForm(data);
+  }
+  e.preventDefault();
+}
+
+//  Cancel Edit State
+function cancelEdit(e) {
+  if(e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
   }
   e.preventDefault();
 }
